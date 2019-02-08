2b126aa68edd41b7b87261cbc11a497cd3f293b9
Milzan Malik <38238691+Skyleaft@users.noreply.github.com>
2019-02-08 21:00:18 +0700

    Add files via upload

--- UAplikasiKoperasi.pas
+++ UAplikasiKoperasi.pas
@@ -2,7 +2,8 @@ program Aplikasi_Koperasi;
 uses crt,sysutils;
 const
      maks=100;
-     maksSimpanan=1000;
+     maksPinjaman=1000;
+     maks_menu=12;
      garis_kiri=5;
      garis_kanan=60;
      garis_atas=3;
@@ -32,11 +33,14 @@ type
                    angsuran:real;
     end;
 
+    data_menu=array[1..maks_menu] of string;
 
 var
    data_anggota:Array[1..maks] of TAnggota;
-   bd,pil:integer;
-
+   data_pinjaman:Array[1..maksPinjaman] of TPinjaman;
+   Tmenu:data_menu;
+   bd,bdpinjam,pil:integer;
+   YYYY,MM,DD : Word;
 
 
 
@@ -49,7 +53,9 @@ var
    i:integer;
 begin
      textcolor(tulisan);textbackground(latar);
-     window(kiri,atas,kanan,bawah);clrscr;window(1,1,80,25);
+     window(kiri,atas,kanan,bawah);
+     clrscr;
+     window(1,1,120,30);
      gotoxy(kiri,atas);write(chr(218));
      gotoxy(kanan,atas);write(chr($BF));
      gotoxy(kiri,bawah);write(#192);
@@ -75,19 +81,37 @@ begin
           gotoxy(i,atas);write(#196);
      end;
      gotoxy(kanan,atas);write(#180);
-end;  
+     writeln;
+end;
+
+procedure bersihin();
+var
+   i:integer;
+begin
+     textcolor(0);textbackground(0);
+     window(1,1,120,30);
+     clrscr;
+end;
 
 //----------------------------------------------------------------------------------------------------------------------
 
 
 
 
+
+
+//----------------------------------------------------------------------------------------------------------------------
+//TAMBAH ANGGOTA
 //----------------------------------------------------------------------------------------------------------------------
 procedure tambah_Anggota;
 begin
+     clrscr;
+     bersihin;
      if bd<maks then
      begin
-          clrscr;
+          kotak(4,50,2,20,BLUE,WHITE,'Tambah Data Anggota');
+          pemisah(4,50,2+2);
+
           bd:=bd+1;
           writeln('Masukan Data Anggota Ke-',bd);
           write('Kode Anggota : ');readln(data_anggota[bd].id_anggota);
@@ -106,6 +130,8 @@ end;
 
 
 
+//----------------------------------------------------------------------------------------------------------------------
+//SIMPAN SALDO
 //----------------------------------------------------------------------------------------------------------------------
 procedure simpanan;
 var
@@ -132,7 +158,7 @@ begin
 
               writeln;
               writeln;
-              write('Tambahkan Saldo : ');read(insaldo);
+              write('Tambahkan Saldo : Rp.');read(insaldo);
               data_anggota[index].saldo:=data_anggota[index].saldo + insaldo;
               writeln('Saldo Akhir    : Rp.',data_anggota[index].saldo:0:2);
               readln;
@@ -146,20 +172,99 @@ end;
 
 
 //----------------------------------------------------------------------------------------------------------------------
+//PENARIKAN SALDO
+//----------------------------------------------------------------------------------------------------------------------
+procedure tarik_saldo;
+var
+   id:string;
+   i,index:integer;
+   ketemu:boolean;
+   insaldo:real;
+   tempsaldo:real;
+begin
+     clrscr;
+     writeln('Penarikan Saldo');
+     writeln('Masukan Data Anggota');
+     write('ID Anggota : ');readln(id);
+     for i:=1 to bd do
+         if id = data_anggota[i].id_anggota then
+         begin
+              ketemu:=true;
+              index:=i;
+         end;
+         if ketemu = true then
+         begin
+              writeln('Nama Anggota : ',data_anggota[index].nama);
+              writeln('Alamat       : ',data_anggota[index].alamat);
+              writeln('Saldo        : Rp.',data_anggota[index].saldo:0:2);
+
+              tempsaldo:= data_anggota[index].saldo;
+              writeln;
+              writeln;
+              write('Ambil Saldo : Rp.');read(insaldo);
+              if insaldo > tempsaldo then
+                 writeln('Saldo Anda Kurang')
+              else
+                  tempsaldo := tempsaldo - insaldo;
+
+              data_anggota[index].saldo:=tempsaldo;
+              writeln('Saldo Akhir    : Rp.',data_anggota[index].saldo:0:2);
+              readln;
+          end
+          else
+              writeln('Data Tidak Ditemukan');
+          readln;
+end;
+
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
+//TAMPIL DATA ANGGOTA
+//----------------------------------------------------------------------------------------------------------------------
 procedure tampil_dataAnggota;
 var
    i:integer;
 begin
      clrscr;
      //writeln('-------------------------------------------------------');
-     gotoxy(6,6);writeln('ID Anggota ',#179,' Nama Anggota ',#179,'   Alamat   ',#179,'      No.Telp      ',#179,' Pekerjaan ',#179);
+     gotoxy(6,6);writeln('ID Anggota ',#179,' Nama Anggota ',#179,'   Alamat   ',#179,'       No.Telp       ',#179,'     Pekerjaan    ',#179,'    Saldo    ');
      for i:=1 to bd do
      begin
-         writeln(data_anggota[i].id_anggota:10,'    ',data_anggota[i].nama:12,'  ',data_anggota[i].alamat:10,'    ',data_anggota[i].no_telp:12,'    ',data_anggota[i].pekerjaan:11,'    Rp.',data_anggota[i].saldo:0:2);
+         writeln(data_anggota[i].id_anggota:10,'     ',data_anggota[i].nama:12,'       ',data_anggota[i].alamat:10,'        ',data_anggota[i].no_telp:12,'        ',data_anggota[i].pekerjaan:11,'       Rp.',data_anggota[i].saldo:0:2);
      end;
      readln;
 end;
 //----------------------------------------------------------------------------------------------------------------------
+
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
+//TAMPIL DATA PINJAMAN
+//----------------------------------------------------------------------------------------------------------------------
+procedure tampil_pinjaman;
+var
+   i:integer;
+begin
+     clrscr;
+     //writeln('-------------------------------------------------------');
+     writeln('  Kode ',#179,' ID Anggota ',#179,'  Tanggal  ',#179,' Nominal Pinjam  ',#179,'    Lama   ',#179,' Bunga ',#179,'      Total      ',#179,'    Angsuran    ');
+     for i:=1 to bdpinjam do
+     begin
+         writeln(' ',data_pinjaman[i].kd_pinjaman,'      ',data_pinjaman[i].id_anggota,'       ',data_pinjaman[i].tgl_pinjam,'    Rp.',data_pinjaman[i].jml_pinjam:0:0,'          ',data_pinjaman[i].lama,' Bulan    ',data_pinjaman[i].bunga:0:0,'%      Rp.',data_pinjaman[i].total:0:0,'        Rp.',data_pinjaman[i].angsuran:0:0);
+     end;
+     readln;
+end;
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
 
 
 
@@ -171,7 +276,7 @@ var
    terpilih:integer;
    tombol:char;
 
-procedure tulis_menu;
+procedure tulis_menuUbah;
 var
    i:integer;
 begin
@@ -222,7 +327,7 @@ begin
      terpilih:=1;
      repeat
      clrscr;
-     tulis_menu;
+     tulis_menuUbah;
 
      tombol:=readkey;
      if tombol=bawah then
@@ -390,19 +495,95 @@ end;
 
 
 
+//----------------------------------------------------------------------------------------------------------------------
+//PINJAMAN
+//----------------------------------------------------------------------------------------------------------------------
+procedure pinjaman;
+var
+   id:string;
+   i,index,lama:integer;
+   ketemu:boolean;
+   tanggal,kdpinjam:string;
+   nominal,bunga,total,angsuran:real;
+   tbunga:real;
+
+
+begin
+     clrscr;
+     writeln('Pinjaman');
+     writeln('Masukan Data Anggota');
+     write('ID Anggota : ');readln(id);
+     for i:=1 to bd do
+         if id = data_anggota[i].id_anggota then
+         begin
+              ketemu:=true;
+              index:=i;
+         end;
+         if ketemu = true then
+         begin
+              writeln('Nama Anggota : ',data_anggota[index].nama);
+              writeln('Alamat       : ',data_anggota[index].alamat);
+              writeln('Saldo        : Rp.',data_anggota[index].saldo:0:2);
+
+
+              writeln;
+              bdpinjam:=bdpinjam+1;
+              kdpinjam:='PJ00';
+              data_pinjaman[bdpinjam].kd_pinjaman:=kdpinjam+IntToStr(bdpinjam);
+              Writeln ('Kode Pinjaman : ',data_pinjaman[bdpinjam].kd_pinjaman);
+
+              data_pinjaman[bdpinjam].id_anggota:=data_anggota[index].id_anggota;
+
+
+              DeCodeDate (Date,YYYY,MM,DD);
+              tanggal:=format('%d/%d/%d',[dd,mm,yyyy]);
+              Writeln ('Tanggal Pinjam : ',tanggal);
+              data_pinjaman[bdpinjam].tgl_pinjam:=tanggal;
+
+              write('Masukan Nominal Pinjam : Rp.');readln(nominal);
+              write('Lama Pinjaman(Bulan) : ');readln(lama);
+              write('Bunga(Persen) : ');readln(bunga);
+              tbunga:=(bunga/100)*nominal;
+
+              total:=nominal+tbunga;
+              writeln('Total : Rp.',total:0:0);
+
+              angsuran:=total/lama;
+              writeln('Angsuran : Rp.',angsuran:0:0);
+
+              data_pinjaman[bdpinjam].jml_pinjam:=nominal;
+              data_pinjaman[bdpinjam].lama:=lama;
+              data_pinjaman[bdpinjam].bunga:=bunga;
+              data_pinjaman[bdpinjam].total:=total;
+              data_pinjaman[bdpinjam].angsuran:=angsuran;
+
+              readln;
+          end
+          else
+              writeln('Data Tidak Ditemukan');
+          readln;
+end;
+//----------------------------------------------------------------------------------------------------------------------
+
+
 
 
 
 //----------------------------------------------------------------------------------------------------------------------
-procedure simpanfileAnggota;
+procedure simpansemuafile;
 var
-   i:integer;
+   i,x:integer;
    f:file of TAnggota;
+   fp:file of TPinjaman;
 begin
      assign(f,'FileAnggota.dat');
      rewrite(f);
      for i:=1 to bd do
          write(f,data_anggota[i]);
+     assign(fp,'FilePinjaman.dat');
+     rewrite(fp);
+     for x:=1 to bdpinjam do
+         write(fp,data_pinjaman[x]);
      writeln(bd,'Data Telah Disimpan Ke File');
      readln;
 end;
@@ -411,9 +592,10 @@ end;
 
 
 //----------------------------------------------------------------------------------------------------------------------
-procedure bacafileAnggota;
+procedure bacasemuafile;
 var
    f:file of TAnggota;
+   fp:file of TPinjaman;
 begin
      if fileexists('FileAnggota.dat') then
      begin
@@ -425,12 +607,30 @@ begin
                read(f,data_anggota[bd]);
           end;
           close(f);
-          writeln('Baca data selesai. Terbaca ',bd,' record');
+          writeln('Baca data selesai. Jumlah Anggota : ',bd,' record');
           readln;
      end
      else
      begin
-          writeln('File Belum Ada. Tidak ada data yang terbaca');readln;
+          write('File Belum Ada. Tidak ada data yang terbaca');readln;
+     end;
+
+     if fileexists('FilePinjaman.dat') then
+     begin
+          assign(fp,'FilePinjaman.dat');
+          reset(fp);
+          while not eof(fp) do
+          begin
+               bdpinjam:=bdpinjam+1;
+               read(fp,data_pinjaman[bdpinjam]);
+          end;
+          close(fp);
+          writeln('Baca data selesai. Jumlah Pinjaman : ',bdpinjam,' record');
+          readln;
+     end
+     else
+     begin
+          write('File Belum Ada. Tidak ada data yang terbaca');readln;
      end;
 
 
@@ -474,40 +674,117 @@ end;
 
 
 //----------------------------------------------------------------------------------------------------------------------
+//MENU
+//----------------------------------------------------------------------------------------------------------------------
+procedure isi_menu;
+begin
+     Tmenu[1]:='  1. Tambah Data Anggota        ';
+     Tmenu[2]:='  2. Tampilkan Data Anggota     ';
+     Tmenu[3]:='  3. Pengurutan Data Anggota    ';
+     Tmenu[4]:='  4. Pencarian Data Anggota     ';
+     Tmenu[5]:='  5. Ubah Data Anggota          ';
+     Tmenu[6]:='  6. Hapus Data Anggota         ';
+     Tmenu[7]:='  7. Simpanan                   ';
+     Tmenu[8]:='  8. Pinjaman                   ';
+     Tmenu[9]:='  9. Penarikan Saldo            ';
+     Tmenu[10]:='  10. Tampilkan Data Pinjaman  ';
+     Tmenu[11]:='  11. Simpan Kedalam File      ';
+     Tmenu[11]:='  12. Keluar                   ';
+end;
+
+procedure tulis_menu;
+var
+   i:integer;
+begin
+for i:=1 to maks_menu do
+     begin
+          if terpilih = i then
+          begin
+               textbackground(4);
+               writeln(Tmenu[i]);
+          end
+          else
+          begin
+               textbackground(blue);
+               writeln(Tmenu[i]);
+
+          end;
+
+     end;
+end;
+
+procedure buka_menu(t:integer);
+begin
+     case t of
+          1:tambah_Anggota;
+          2:tampil_dataAnggota;
+          3:;
+          4:;
+          5:pilih_AnggotaUbah;
+          6:pilih_AnggotaHapus;
+          7:simpanan;
+          8:pinjaman;
+          9:tarik_saldo;
+          10:tampil_pinjaman;
+          11:simpansemuafile;
+     end;
+end;
+
+procedure seleksi_menu;
+begin
+     terpilih:=1;
+     repeat
+           clrscr;
+           isi_menu;
+           tulis_menu;
+
+           tombol:=readkey;
+           if tombol=bawah then
+           begin
+                if terpilih < maks_menu then
+                   terpilih:=terpilih+1;
+           end
+           else if tombol=atas then
+           begin
+                terpilih:=terpilih-1;
+                if terpilih = 0 then
+                   terpilih:=terpilih+1;
+           end
+           else if tombol=enter then
+           begin
+                clrscr;
+                buka_menu(terpilih);
+           end;
+
+     until tombol=enter;
+end;
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
 //MULAI DISINI
 //----------------------------------------------------------------------------------------------------------------------
 begin
      bd:=0;
-     bacafileAnggota;
+     bdpinjam:=0;
+     bacasemuafile;
+
      repeat
-           //kotak(garis_kiri,garis_kanan,garis_atas,garis_bawah,BLUE,WHITE,'Aplikasi Koperasi');
-           //pemisah(garis_kiri,garis_kanan,garis_atas+2);
-           clrscr;
-           writeln('1. Tambah Data Anggota');
-           writeln('2. Tampilkan Data');
-           writeln('3. Pengurutan');
-           writeln('4. Pencarian');
-           writeln('5. Simpan');
-           writeln('6. Simpanan');
-           writeln('7. Ubah Data Anggota');
-           writeln('8. Hapus Data Anggota');
-           writeln('0. Keluar');
-           writeln;
-           write('Pilih(1-5) : ');readln(pil);
+     bersihin;
+     clrscr;
+     kotak(3,70,2,20,BLUE,WHITE,'Aplikasi Koperasi');
+     pemisah(3,70,4);
 
-           case pil of
-           1:tambah_Anggota;
-           2:tampil_dataAnggota;
-           3:;
-           4:;
-           5:simpanfileAnggota;
-           6:simpanan;
-           7:pilih_AnggotaUbah;
-           8:pilih_AnggotaHapus;
-           end;
+     window(5,5,67,19);
+     seleksi_menu;
 
-     until pil=0;
      readln;
-     simpanfileAnggota;
+     until terpilih=12;
+     simpansemuafile;
      readln;
 end.
